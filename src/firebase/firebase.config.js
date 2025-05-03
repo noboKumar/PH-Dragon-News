9c5eee6c5a970e16d9022bdc40dedae8eeea62de
noboKumar <nobokumar227@gmail.com>
2025-05-03 14:05:32 +0600

    remove firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_s_uBf2omlfKcpTxfxMob3T0RXIqhXSg",
-  authDomain: "dragon-news-227.firebaseapp.com",
-  projectId: "dragon-news-227",
-  storageBucket: "dragon-news-227.firebasestorage.app",
-  messagingSenderId: "161456139850",
-  appId: "1:161456139850:web:2cfa20e38caa96ab6580b3"
+
 };
 
 const app = initializeApp(firebaseConfig);
